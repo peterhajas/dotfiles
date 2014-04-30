9cd7d1b95af1bd08b9bf0eb90801b38a0b03b3cb
Peter Hajas <peterhajas@gmail.com>
2014-04-29 23:08:49 -0700

    Use `vimdiff` as the default diff / merge tool. Otherwise, use `ksdiff`.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,6 +18,21 @@ alias confedit "mate ~/.config/fish/config.fish"
 
 set fish_greeting hello
 
+# Tool configuration
+
+## Default diff/merge tool is vimdiff
+
+set diffMergeTool "vimdiff"
+
+if test -d /Applications/Kaleidoscope.app
+    ## But if we have ksdiff, let's use that
+    set PATH $PATH /Applications/Kaleidoscope.app/Contents/Resources/bin;
+    set diffMergeTool "Kaleidoscope"
+end
+
+git config --global diff.tool $diffMergeTool
+git config --global merge.tool $diffMergeTool
+
 # Prompt Stuff
 
 ## Version Control Status
