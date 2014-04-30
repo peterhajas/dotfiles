54092ff3eab27f99a838be2ef2e57e7cbf45d3be
Peter Hajas <peterhajas@gmail.com>
2014-04-29 22:10:43 -0700

    Updated update script to install homebrew if it's not already installed

--- update.sh
+++ update.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-echo "Updating pathogen..."
+echo "Installing Homebrew..."
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
-rm vim/.vim/autoload/pathogen.vim
+echo "Updating pathogen..."
+rm -f vim/.vim/autoload/pathogen.vim
 curl -Sso vim/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-echo "Updating vim plugins"
+echo "Updating vim plugins..."
 git submodule init
 git submodule update
