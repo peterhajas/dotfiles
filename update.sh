a53f42dba8c3d8b9d49708da6269104ee93d23dc
Peter Hajas <peterhajas@gmail.com>
2014-04-30 00:03:48 -0700

    update.sh now only installs homebrew/stow if they're not already installed. Also, it re-stows all apps.

--- update.sh
+++ update.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
+
 echo "Installing Homebrew..."
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+if which brew 2>/dev/null 1>/dev/null; then
+    echo "Homebrew already installed"
+else
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
 
 echo "Updating pathogen..."
 curl -Sso vim/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+echo "Installing stow..."
+if which stow 2>/dev/null 1>/dev/null; then
+    echo "stow already installed"
+else
+    brew install stow
+fi
+
+echo "Restowing all apps..."
+for dir in */
+do
+    echo Unstowing $dir
+    stow -D $dir
+    echo Restowing $dir
+    stow $dir
+done
 
 echo "Updating vim plugins..."
 git submodule init
