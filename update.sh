c3eedfcd6537c093885351db8b0e1adb8e367fef
Peter Hajas <peterhajas@gmail.com>
2014-08-03 23:58:41 -0700

    Moved off of brewfiles, as they would error out if a package was already installed.

--- update.sh
+++ update.sh
@@ -7,16 +7,53 @@ else
     ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 fi
 
-echo "Executing brewfiles..."
+echo "Installing Homebrew software..."
 
-echo "Brewfile: Installing utilities..."
-brew bundle homebrew/.brewfile
+echo "Homebrew: Installing utilities..."
 
-echo "Brewfile: Installing Cask apps..."
-brew bundle homebrew/.caskfile
+brew install fish
+brew install stow
+brew install tmux
+brew install reattach-to-user-namespace
+brew install ctags --HEAD
+brew install hg
+brew install lighttpd
+brew install wget
+brew install htop
+brew install fortune
+brew install wine
+brew install sloccount
 
-echo "Brewfile: Installing "fun" Cask apps..."
-brew bundle homebrew/.caskfile_fun
+echo "Homebrew: Installing Cask apps..."
+
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install macvim
+brew cask install spotify
+brew cask install bartender
+brew cask install flux
+brew cask install keyboard-maestro
+brew cask install keyremap4macbook
+brew cask install dropbox
+brew cask install slate
+brew cask install vlc
+brew cask install audacity
+brew cask install textexpander
+brew cask install sketch
+brew cask install kaleidoscope
+brew cask install xquartz
+brew cask install hazel
+
+echo "Homebrew: Installing "fun" Cask apps..."
+
+brew cask install steam
+brew cask install origin
+brew cask install teamspeak-client
+
+echo "Finalizing Homebrew installation..."
+brew cask cleanup
+brew update
+brew doctor
 
 echo "Configuring stow..."
 stow -D stow
