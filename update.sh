caf26969b5f1493647d58590dbc0e6ae7e9d8862
Peter Hajas <peterhajas@gmail.com>
2014-10-16 22:31:56 -0700

    Updated homebrew path

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ echo "Installing Homebrew..."
 if which brew 2>/dev/null 1>/dev/null; then
     echo "Homebrew already installed."
 else
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 echo "Installing Homebrew software..."
