61bbcf59482f6be754252938256cee619748fba8
Peter Hajas <peterhajas@gmail.com>
2014-11-04 22:18:46 -0800

    Organized the OSX Defaults script

--- .osx_defaults.bash
+++ .osx_defaults.bash
@@ -1,41 +1,50 @@
-# Set highlight color to orange
+# Global settings
+
+## Set highlight color to orange
 defaults write NSGlobalDomain AppleHighlightColor -string "1.0 0.448 0.0"
 
-# Use Dark Mode
+## Use Dark Mode
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 
-# Expand save panel by default
+# Finder / File Panels
+
+## Expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-# Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-# Hide icons for hard drives, servers, and removable media on the desktop
+## Hide icons for hard drives, servers, and removable media on the desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 
-# Finder: show all filename extensions
+## Finder: show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Disable the warning when changing a file extension
+## Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Show the ~/Library folder
-chflags nohidden ~/Library
+# Dock
 
-# Don’t automatically rearrange Spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false
-
-# Don't hide the Dock
+## Don't hide the Dock
 defaults write com.apple.dock autohide -bool false
 
-# Make Dock icons of hidden applications translucent
+## Make Dock icons of hidden applications translucent
 defaults write com.apple.dock showhidden -bool true
 
-# Hot corners
+# Desktop / Screensaver
+
+## Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+## Show the ~/Library folder
+chflags nohidden ~/Library
+
+## Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+## Hot corners
 # Possible values:
 #  0: no-op
 #  2: Mission Control
@@ -47,41 +56,44 @@ defaults write com.apple.dock showhidden -bool true
 # 10: Put display to sleep
 # 11: Launchpad
 # 12: Notification Center
-# Top left screen corner: nothing
 
+### Top left screen corner: nothing
 defaults write com.apple.dock wvous-tl-corner -int 0
 defaults write com.apple.dock wvous-tl-modifier -int 0
-# Top right screen corner: nothing
 
+### Top right screen corner: nothing
 defaults write com.apple.dock wvous-tr-corner -int 0
 defaults write com.apple.dock wvous-tr-modifier -int 0
-# Bottom left screen corner: start screen saver
 
+### Bottom left screen corner: start screen saver
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
-# Use DuckDuckGo as my search engine
+# Accessibility
 
-defaults write com.apple.Safari SearchProviderIdentifier "com.duckduckgo"
-defaults write com.apple.Safari SearchProviderIdentifierMigratedToSystemPreference -int 0
-
-# Enable Safari’s debug menu
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-
-# Enable the Develop menu and the Web Inspector in Safari
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-
-# Add a context menu item for showing the Web Inspector in web views
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-# Enable continuous zoom with the pointer
+## Enable continuous zoom with the pointer
 defaults write com.apple.AppleMultitouchTrackpad HIDScrollZoomModifierMask 262144
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad 262144
 
 defaults write com.apple.universalaccess closeViewPanningMode 0
 defaults write com.apple.universalaccess closeViewScrollWheelToggle 1
+
+# Safari
+
+## Use DuckDuckGo as my search engine
+defaults write com.apple.Safari SearchProviderIdentifier "com.duckduckgo"
+defaults write com.apple.Safari SearchProviderIdentifierMigratedToSystemPreference -int 0
+
+## Enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+## Enable the Develop menu and the Web Inspector in Safari
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+## Add a context menu item for showing the Web Inspector in web views
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 killall Finder
 killall Safari
