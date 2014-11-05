4e9a1da4cfa7885b0ee39b47de5fd677c9304d82
Peter Hajas <peterhajas@gmail.com>
2014-11-04 20:09:18 -0800

    Don't show anything if the current directory is not tracked in version control

--- fish/.config/fish/functions/prompt_current_vcs_system.fish
+++ fish/.config/fish/functions/prompt_current_vcs_system.fish
@@ -1,6 +1,6 @@
 function prompt_current_vcs_system
     git branch >/dev/null 2>/dev/null; and echo -n '±'; and return
-    # hg root >/dev/null 2>/dev/null; and echo -n '☿'; and return
+    hg root >/dev/null 2>/dev/null; and echo -n '☿'; and return
     svn info >/dev/null 2>/dev/null; and echo -n '§'; and return
-    echo '○'
+    return
 end
