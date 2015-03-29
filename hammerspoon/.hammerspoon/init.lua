b46c66226ff1fd8725ffd6308fad21e5f19385e0
Peter Hajas <peterhajas@gmail.com>
2015-03-28 19:38:25 -0700

    Tear down timers and watchers when reloading

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -394,6 +394,9 @@ batteryWatcher:start()
 -- http://www.hammerspoon.org/go/#fancyreload
 function reload_config(files)
     destroyiTunesTrackDisplay()
+    appWatcher:stop()
+    timer:stop()
+    batteryWatcher:stop()
     hs.reload()
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
