fc39c56681f7b994bac85a3c305d9d93d8c7b293
Peter Hajas <peterhajas@gmail.com>
2015-08-10 21:09:05 -0700

    Add an experimental new iTunes track display to the desktop. This uses AppleScript to consult iTunes for the current album artwork. This could eventually be an hs.itunes enhancement.

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -410,6 +410,91 @@ hs.hotkey.bind(hyper, "4", function()
 end)
 
 -- }}}
+-- iTunes Album Artwork (a test) {{{
+
+function currentiTunesArtwork()
+    if hs.appfinder.appFromName('iTunes') then
+        applescript_str = [[global f, a
+        tell application "iTunes"
+            
+            tell artwork 1 of current track
+                set d to raw data
+                if format is «class PNG » then
+                    set x to "png"
+                else
+                    set x to "jpg"
+                end if
+            end tell
+            
+            set a to album of current track
+        end tell
+
+        set f to (((path to temporary items) as text) & "cover." & x)
+        set b to open for access file result with write permission
+        set eof b to 0
+        write d to b
+        close access b
+
+        return f]]
+
+        ok, result = hs.applescript.applescript(applescript_str)
+
+        if ok == true then
+            path = string.gsub(result, ":", "/")
+
+            -- Applescript paths include the name of the disk as the first
+            -- component of the path. We should shave this off, so that calls
+            -- to hs.image.imageFromPath() can find images
+
+            local path_elem = hs.fnutils.split(path, "/")
+            local disk_name = path_elem[1]
+
+            -- Remove this leading disk_name from path
+
+            path = string.gsub(path, disk_name, "")
+
+            image = hs.image.imageFromPath(path)
+
+            return image
+        else
+            return nil
+        end
+    else
+        return nil
+    end
+end
+
+local itunesArtwork
+
+function updateiTunesArtwork()
+    local image = currentiTunesArtwork()
+
+    if image ~= nil then
+        itunesArtwork:setImage(image)
+        itunesArtwork:show()
+    else
+        itunesArtwork:hide()
+    end
+end
+
+function buildiTunesArtwork()
+    if itunesArtwork ~= nil then itunesArtwork:delete() end
+    local preferredScreenFrame = preferredScreen():fullFrame()
+    local dimension = 300
+    local frame = hs.geometry.rect(preferredScreenFrame.x + regularDecorationPadding(),
+                                   preferredScreenFrame.h - dimension - 35,
+                                   300,
+                                   300)
+    itunesArtwork = hs.drawing.image(frame, "ASCII:.")
+
+    itunesArtwork:sendToBack()
+
+    updateiTunesArtwork()
+end
+
+buildiTunesArtwork()
+
+-- }}}
 -- iTunes Current Track Status Update{{{
 
 function updateiTunesTrackDisplay()
@@ -419,6 +504,8 @@ function updateiTunesTrackDisplay()
         local trackName = hs.itunes.getCurrentTrack()
         local artistName = hs.itunes.getCurrentArtist()
         statusText = trackName .. ' by ' .. artistName
+
+        updateiTunesArtwork()
     end
 
     return statusText
