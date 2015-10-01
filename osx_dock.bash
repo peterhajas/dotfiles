0fba111561e261fa3d5e4e006f711b36653da7f4
Peter Hajas <peterhajas@gmail.com>
2015-10-01 00:14:57 -0700

    Reeder joins the Dock

--- osx_dock.bash
+++ osx_dock.bash
@@ -16,6 +16,7 @@ $DOCKUTILADD /Applications/Safari.app
 $DOCKUTILADD /Applications/Utilities/Terminal.app
 $DOCKUTILADD /Applications/OmniFocus.app
 $DOCKUTILADD /Applications/Tweetbot.app
+$DOCKUTILADD /Applications/Reeder.app
 $DOCKUTILADD /Applications/iTunes.app
 $DOCKUTILADD /Applications/Photos.app
 $DOCKUTILADD /Applications/Messages.app
