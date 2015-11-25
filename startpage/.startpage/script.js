50df7fb6a23399aa0def384b977e730715bd073b
Peter Hajas <peterhajas@gmail.com>
2015-11-24 23:39:54 -0500

    Support collapsing panels now

--- startpage/.startpage/script.js
+++ startpage/.startpage/script.js
@@ -4,10 +4,16 @@ function prepareEventHandlers()
     console.log('test');
 
     $('.linkSectionContainer').click(function() {
+        var isCollapsed = $(this).hasClass("collapsed");
         // Collapse all other columns
         $('.linkSectionContainer').addClass("collapsed");
-        // Open this column
-        $(this).removeClass("collapsed");
+        // Toggle this column
+        if (isCollapsed) {
+            $(this).removeClass("collapsed");
+        }
+        else {
+            $(this).addClass("collapsed");
+        }
     });
 
     $('.linkSectionContainer').addClass("collapsed");
