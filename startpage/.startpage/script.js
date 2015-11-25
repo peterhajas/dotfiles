99a1db818c7ff347fa78681251ac9676a00b5900
Peter Hajas <peterhajas@gmail.com>
2015-11-24 23:44:51 -0500

    Now, you can hover over startpage sections

--- startpage/.startpage/script.js
+++ startpage/.startpage/script.js
@@ -3,6 +3,12 @@ function prepareEventHandlers()
 {
     console.log('test');
 
+    $('.linkSectionContainer').hover(function() {
+        $(this).removeClass("collapsed");
+    }, function() {
+        $(this).addClass("collapsed");
+    });
+
     $('.linkSectionContainer').click(function() {
         var isCollapsed = $(this).hasClass("collapsed");
         // Collapse all other columns
