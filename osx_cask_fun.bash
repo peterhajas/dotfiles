5241b91072f645bdfdefb0f0dfecf692925bf08b
Peter Hajas <peterhajas@gmail.com>
2015-12-08 21:57:03 -0800

    More dungeon crawlers

--- osx_cask_fun.bash
+++ osx_cask_fun.bash
@@ -4,3 +4,5 @@ brew cask install steam
 brew cask install origin
 brew cask install openemu
 brew cask install xbox360-controller-driver
+brew cask install dungeon-crawl-stone-soup-console
+brew cask install dungeon-crawl-stone-soup-tiles
