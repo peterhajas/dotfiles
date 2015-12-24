1ba05087372fe0690dc325d91668e510e0a62a93
Peter Hajas <peterhajas@gmail.com>
2015-12-24 10:05:10 -0600

    Fix music.sh check for iTunes running

--- status/bin/status/music.sh
+++ status/bin/status/music.sh
@@ -2,7 +2,7 @@
 
 itunesRunning=`ps -ax |grep iTunes | grep -v Helper | grep -v "grep" | wc -l`
 
-if test "$itunesRunning -gt 0"
+if [ $itunesRunning -gt 0 ]
 then
     album=`osascript -e "tell application \"iTunes\" to album of the current track" 2>/dev/null`
     artist=`osascript -e "tell application \"iTunes\" to artist of the current track" 2>/dev/null`
@@ -10,8 +10,10 @@ then
 
     trackLength=`echo $track | wc -c`
 
-    if test "$trackLength -gt 1"
+    if [ $trackLength -gt 1 ]
     then
         echo "$artist - $track" | tr -d '\n'
+    else
+        echo "[stopped]"
     fi
 fi
