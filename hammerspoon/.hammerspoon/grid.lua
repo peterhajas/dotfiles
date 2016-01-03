71cdccc9be908dd4982e378616f8230fa282cb0c
Peter Hajas <peterhajas@gmail.com>
2016-01-02 22:19:31 -0700

    Add margins to the grid

--- hammerspoon/.hammerspoon/grid.lua
+++ hammerspoon/.hammerspoon/grid.lua
@@ -3,6 +3,10 @@ require "hyper"
 hs.window.animationDuration = 0
 hs.grid.GRIDWIDTH = 6
 hs.grid.GRIDHEIGHT = 2
+
+local margins = hs.geometry.size(4, 4)
+
+hs.grid.setMargins(margins)
 hs.grid.ui.showExtraKeys = false
 
 hs.hotkey.bind(hyper, "space", function()
