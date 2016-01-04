93ad73a91b3523df3a7f8bab1643e4b2256662a9
Peter Hajas <peterhajas@gmail.com>
2016-01-04 01:09:03 -0800

    Generate grid parameters automatically based on screens

--- hammerspoon/.hammerspoon/grid.lua
+++ hammerspoon/.hammerspoon/grid.lua
@@ -1,8 +1,36 @@
 require "hyper"
 
 hs.window.animationDuration = 0
-hs.grid.GRIDWIDTH = 6
-hs.grid.GRIDHEIGHT = 2
+
+-- Found by taking the largest res I run at (3200) and diving by hint keys on
+-- the keyboard
+local screenWidthPerGridUnit = 3200 / 10
+-- Likewise with height (1800 is the largest)
+local screenHeightPerGridUnit = 1800 / 4
+
+function updateGridForScreen(screen)
+    local w = screen:fullFrame().w
+    local h = screen:fullFrame().h
+
+    w = w / screenWidthPerGridUnit
+    h = h / screenHeightPerGridUnit
+
+    w = math.floor(w)
+    h = math.floor(h)
+
+    size = hs.geometry.size(w, h)
+
+    hs.grid.setGrid(size, screen)
+end
+
+function updateGridsForScreens()
+    screens = hs.screen.allScreens()
+    for i, screen in ipairs(screens) do
+        updateGridForScreen(screen)
+    end
+end
+
+updateGridsForScreens()
 
 local margins = hs.geometry.size(4, 4)
 
