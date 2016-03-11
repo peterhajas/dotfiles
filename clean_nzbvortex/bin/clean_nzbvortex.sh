56f5b1ee69cade7ca7d9245960c614d86816e9c5
Peter Hajas <peterhajas@gmail.com>
2016-03-10 20:43:50 -0800

    Add a script to clean up an app if it goes awry

---
+++ clean_nzbvortex/bin/clean_nzbvortex.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -r "$HOME/Library/Saved Application State/com.VortexApps.NZBVortex3.savedState"
+rm -r "$HOME/Library/Preferences/com.VortexApps.NZBVortex3.plist"
+rm -r "$HOME/Library/Containers/com.VortexApps.NZBVortex3"
