015b03377decfa02c3b9325868c9675fa14d6bd3
Peter Hajas <peterhajas@gmail.com>
2016-12-29 23:28:50 -0600

    Move play to be a bash script instead of a fish function

--- fish/.config/fish/functions/play.fish
+++
@@ -1,5 +0,0 @@
-# Inspired by https://github.com/exogen/dotfiles/blob/730fb8e2b72b2fc3aa3d90b889874bb5195e1d07/.profile#L65
-function play
-    set OUTPATH "$TMPDIR/%(title)s-%(id)s.%(ext)s"
-    youtube-dl --default-search="ytsearch:" --format="bestaudio[ext!=webm]" --output $OUTPATH --exec afplay "$argv"
-end
