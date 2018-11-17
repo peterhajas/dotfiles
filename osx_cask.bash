33545a172ef316ed07cb49cbff921dbdc1e4a640
Peter Hajas <peterhajas@gmail.com>
2018-11-17 11:47:14 -0600

    Remove some apps I never use

--- osx_cask.bash
+++ osx_cask.bash
@@ -3,13 +3,10 @@
 brew install caskroom/cask/brew-cask
 brew cask install google-chrome
 brew cask install macvim
-brew cask install dropbox
 brew cask install vlc
 brew cask install audacity
-brew cask install textexpander
 brew cask install sketch
 brew cask install xquartz
-brew cask install hazel
 brew cask install hex-fiend
 brew cask install appcleaner
 brew cask install daisydisk
