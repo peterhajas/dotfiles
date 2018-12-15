602f6c7e24384d05768ef38e21649559d05e695b
Peter Hajas <peterhajas@gmail.com>
2018-12-15 13:52:32 -0800

    Add mpsyt to my homebrew installs

--- osx_homebrew.bash
+++ osx_homebrew.bash
@@ -27,5 +27,6 @@ brew install homebrew/games/nethack
 brew install tree
 brew install jq
 brew install ag
+brew install mpsyt
 
 # Hi Peter! Are you adding software here? If so, make sure to add it to linux_apt_get.bash if it makes sense. Sincerely, Past Peter
