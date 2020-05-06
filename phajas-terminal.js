5b9d5af5d83ae83307ffdf27db9ff2ad9340bf86
Peter Hajas <peterhajas@gmail.com>
2020-05-05 21:34:50 -0500

    Add Blink version of my terminal theme

---
+++ phajas-terminal.js
@@ -0,0 +1,4 @@
+"background-color": "#2d2d2d",
+"foreground-color": "#d3d0c8",
+"cursor-color":     "#d3d0c8",
+"color-palette-overrides": ["#2d2d2d","#f2777a","#99cc99","#ffcc66","#6699cc","#cc99cc","#66cccc","#d3d0c8","#747369","#f2777a","#99cc99","#ffcc66","#6699cc","#cc99cc","#66cccc","#f2f0ec"]
