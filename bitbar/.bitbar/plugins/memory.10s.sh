5c31719b04096012c2406ac381bb6ac7d9f44bce
Peter Hajas <peterhajas@gmail.com>
2020-08-20 23:16:25 -0700

    New bitbar plugins for CPU and memory usage

--- bitbar/.bitbar/plugins/memory.10s.sh
+++ bitbar/.bitbar/plugins/memory.10s.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # based on https://github.com/koekeishiya/kwm/issues/8
 
-OUTPUT=`ps -A -o %mem | awk '{s+=$1} END {print "mem " s}' | tr -d '\n'`
-echo "$OUTPUT | color=#6699cc"
+OUTPUT=`ps -A -o %mem | awk '{s+=$1} END {print "🧠 " s}' | tr -d '\n'`
+echo "$OUTPUT"
