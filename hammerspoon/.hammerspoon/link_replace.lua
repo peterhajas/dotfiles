a6e2eb49fea9dc9ab9a98516bfd3b7b7e74be6b0
Peter Hajas <peterhajas@gmail.com>
2020-10-22 18:08:30 -0700

    Update link_replace for potential null strings

--- hammerspoon/.hammerspoon/link_replace.lua
+++ hammerspoon/.hammerspoon/link_replace.lua
@@ -1,6 +1,9 @@
 -- Replaces links that are found on the clipboard, if applicable
 
 function replacePasteboardLinkIfNecessary(contents)
+    if contents == nil then
+        return
+    end
     if string.find(contents, 'twitter.com') then
         -- Replace twitter.com with nitter.net
         newContents = contents:gsub('twitter.com', 'nitter.net')
