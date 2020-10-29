6765bcbe3c9b9a3d34378321841e82fe59e64ca6
Peter Hajas <peterhajas@gmail.com>
2020-10-29 00:07:37 -0700

    Remove audiodevice alerts

--- hammerspoon/.hammerspoon/audio_output.lua
+++ hammerspoon/.hammerspoon/audio_output.lua
@@ -4,18 +4,6 @@ local lastSeenName = ""
 
 hs.audiodevice.watcher.setCallback(function(kind)
     streamdeck_updateButton('audio')
-    if kind == "dOut" or kind == "sOut" then
-        local name = hs.audiodevice.current()['name']
-        if name ~= lastSeenName then
-            lastSeenName = name
-            local volume = hs.audiodevice.current()['volume']
-            if volume == nil then
-                volume = ""
-            end
-            local alertText = "🔉 " .. name .. " " .. volume
-            hs.alert(alertText, hs.screen.primaryScreen())
-        end
-    end
 end)
 
 hs.audiodevice.watcher.start()
