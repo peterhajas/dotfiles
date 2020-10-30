e88f39c74560547ddb958b25e74476358d2cdec1
Peter Hajas <peterhajas@gmail.com>
2020-10-29 19:35:48 -0700

    Add page indicator to audio input switcher buttons

--- hammerspoon/.hammerspoon/streamdeck_buttons/audio_devices.lua
+++ hammerspoon/.hammerspoon/streamdeck_buttons/audio_devices.lua
@@ -1,5 +1,34 @@
 require "streamdeck_buttons.button_images"
 
+local function allAudioDevices(input)
+    local devices
+    if input then
+        devices = hs.audiodevice.allInputDevices()
+    else
+        devices = hs.audiodevice.allOutputDevices()
+    end
+    return devices
+end
+
+local function indexAndCountOfAudioDevices(input)
+    local allDevices = allAudioDevices(input)
+    local deviceName = hs.audiodevice.current(input)['name']
+    local index = -1
+    local count = 0
+
+    for k,v in pairs(allDevices) do
+        if v:name() == deviceName then
+            index = k
+        end
+        count = count + 1
+    end
+
+    return {
+        ['index'] = index,
+        ['count'] = count
+    }
+end
+
 -- Returns a button for controlling audio devices
 -- `input` controls whether this is an input device
 --         or an output device button
@@ -13,33 +42,41 @@ function audioDeviceButton(input)
     return {
         ['imageProvider'] = function (pressed)
             local deviceName = hs.audiodevice.current(input)['name']
-            local text = ""
+            local indexAndCount = indexAndCountOfAudioDevices(input)
+            local empty = "􀀀"
+            local full = ""
             if input then
-                text = "􀊱\n" .. deviceName
+                full = "􀊱"
             else
-                text = "􀊩\n" .. deviceName
+                full = "􀊩"
             end
-            return streamdeck_imageFromText(text, { ['fontSize'] = 25 })
+            local pageIndicator = ""
+            local fillProgress = 1
+
+            -- Add in empty circles until we hit the selected index
+            while fillProgress < indexAndCount['index'] do
+                pageIndicator = pageIndicator .. empty
+                fillProgress = fillProgress + 1
+            end
+
+            -- Add in full circle
+            pageIndicator = pageIndicator .. full
+            fillProgress = fillProgress + 1
+
+            -- Add in empty circles until we hit the count
+            while fillProgress <= indexAndCount['count'] do
+                pageIndicator = pageIndicator .. empty
+                fillProgress = fillProgress + 1
+            end
+
+            local text = '\n' .. pageIndicator .. '\n' .. deviceName
+            return streamdeck_imageFromText(text, { ['fontSize'] = 15 })
         end,
         ['pressUp'] = function()
-            local currentDeviceName = hs.audiodevice.current(input)['name']
-
-            local allDevices = { }
-            if input then
-                allDevices = hs.audiodevice.allInputDevices()
-            else
-                allDevices = hs.audiodevice.allOutputDevices()
-            end
-
-            local index = -1
-            local count = 0
-
-            for k,v in pairs(allDevices) do
-                if v:name() == currentDeviceName then
-                    index = k
-                end
-                count = count + 1
-            end
+            local allDevices = allAudioDevices(input)
+            local indexAndCount = indexAndCountOfAudioDevices(input)
+            local index = indexAndCount['index']
+            local count = indexAndCount['count']
 
             if index ~= -1 then
                 index = index + 1
