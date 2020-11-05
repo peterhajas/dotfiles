84fe594ab7a9f3952b65c5701e3f17a47e7fe13d
Peter Hajas <peterhajas@gmail.com>
2020-11-04 20:01:54 -0800

    Some improvements to album art

--- hammerspoon/.hammerspoon/itunes_albumart.lua
+++ hammerspoon/.hammerspoon/itunes_albumart.lua
@@ -1,54 +1,57 @@
 -- Grab the current track's iTunes albumart
 function currentiTunesArtwork()
+    local app = hs.application'Music'
+    if app == nil then
+        return nil
+    end
+    if app:isRunning() == false then
+        return nil
+    end
     if hs.itunes.getCurrentTrack() == nil then
         return nil
     end
-    if hs.appfinder.appFromName('Music') then
-        applescript_str = [[global f, a
-        tell application "Music"
-            
-            tell artwork 1 of current track
-                set d to raw data
-                if format is «class PNG » then
-                    set x to "png"
-                else
-                    set x to "jpg"
-                end if
-            end tell
-            
-            set a to album of current track
+    applescript_str = [[global f, a
+    tell application "Music"
+        
+        tell artwork 1 of current track
+            set d to raw data
+            if format is «class PNG » then
+                set x to "png"
+            else
+                set x to "jpg"
+            end if
         end tell
+        
+        set a to album of current track
+    end tell
 
-        set f to (((path to temporary items) as text) & "cover." & x)
-        set b to open for access file result with write permission
-        set eof b to 0
-        write d to b
-        close access b
+    set f to (((path to temporary items) as text) & "cover." & x)
+    set b to open for access file result with write permission
+    set eof b to 0
+    write d to b
+    close access b
 
-        return f]]
+    return f]]
 
-        ok, result = hs.applescript.applescript(applescript_str)
+    ok, result = hs.applescript.applescript(applescript_str)
 
-        if ok == true then
-            path = string.gsub(result, ":", "/")
+    if ok == true then
+        path = string.gsub(result, ":", "/")
 
-            -- Applescript paths include the name of the disk as the first
-            -- component of the path. We should shave this off, so that calls
-            -- to hs.image.imageFromPath() can find images
+        -- Applescript paths include the name of the disk as the first
+        -- component of the path. We should shave this off, so that calls
+        -- to hs.image.imageFromPath() can find images
 
-            local path_elem = hs.fnutils.split(path, "/")
-            local disk_name = path_elem[1]
+        local path_elem = hs.fnutils.split(path, "/")
+        local disk_name = path_elem[1]
 
-            -- Remove this leading disk_name from path
+        -- Remove this leading disk_name from path
 
-            path = string.gsub(path, disk_name, "")
+        path = string.gsub(path, disk_name, "")
 
-            image = hs.image.imageFromPath(path)
+        image = hs.image.imageFromPath(path)
 
-            return image
-        else
-            return nil
-        end
+        return image
     else
         return nil
     end
