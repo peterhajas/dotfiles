ebd6ff6f6e13687a9dbe1de5669016ab5367d00a
Peter Hajas <peterhajas@gmail.com>
2020-11-10 23:08:54 -0800

    Tween between green and red for the CPU and memory buttons

--- hammerspoon/.hammerspoon/color_support.lua
+++ hammerspoon/.hammerspoon/color_support.lua
@@ -26,11 +26,9 @@ end
 -- Returns a color mapping the floating point value from `fraction` to a
 -- representative color. `fraction` is assumed to be in the range 0 to 1 (inc.)
 function severityColorForFraction(fraction)
-    if fraction <= 0.4 then
-        return hs.drawing.color.lists()['hammerspoon']['osx_green']
-    end
-    if fraction <= 0.85 then
-        return hs.drawing.color.lists()['hammerspoon']['osx_yellow']
-    end
-    return hs.drawing.color.lists()['hammerspoon']['osx_red']
+    local green = hs.drawing.color.lists()['hammerspoon']['osx_green']
+    local red = hs.drawing.color.lists()['hammerspoon']['osx_red']
+    local low = green
+    local high = red
+    return colorBetween(low, high, fraction)
 end
