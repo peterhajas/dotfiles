297d2dc68f707856284174881e62810c9b6ce1bf
Peter Hajas <peterhajas@gmail.com>
2020-11-10 23:05:51 -0800

    Neater looking weather button

--- hammerspoon/.hammerspoon/color_support.lua
+++ hammerspoon/.hammerspoon/color_support.lua
@@ -1,3 +1,28 @@
+-- Lerps between `a` and `b` by `fraction`
+function lerp(a, b, fraction)
+    local range = b - a
+    return a + (range * fraction)
+end
+
+-- Returns a color between the two colors at `fraction`
+-- `fraction` is assumed to be in the range 0 to 1 (inclusive)
+function colorBetween(color1, color2, fraction)
+    local color1HSB = hs.drawing.color.asHSB(color1)
+    local color2HSB = hs.drawing.color.asHSB(color2)
+
+    local alpha = lerp(color1HSB['alpha'], color2HSB['alpha'], fraction)
+    local brightness = lerp(color1HSB['brightness'], color2HSB['brightness'], fraction)
+    local hue = lerp(color1HSB['hue'], color2HSB['hue'], fraction)
+    local saturation = lerp(color1HSB['saturation'], color2HSB['saturation'], fraction)
+
+    return {
+        ['alpha'] = alpha,
+        ['brightness'] = brightness,
+        ['hue'] = hue,
+        ['saturation'] = saturation,
+    }
+end
+
 -- Returns a color mapping the floating point value from `fraction` to a
 -- representative color. `fraction` is assumed to be in the range 0 to 1 (inc.)
 function severityColorForFraction(fraction)
--- hammerspoon/.hammerspoon/streamdeck_buttons/url.lua
+++ hammerspoon/.hammerspoon/streamdeck_buttons/url.lua
@@ -1,4 +1,5 @@
 require "streamdeck_buttons.button_images"
+require "color_support"
 
 local function urlButton(url, button)
     local out = button
@@ -15,7 +16,15 @@ end
 weatherButton = urlButton('https://wttr.in', {
     ['imageProvider'] = function()
         local output = hs.execute('curl -s "wttr.in?format=1" | sed "s/+//" | sed "s/F//" | grep -v "Unknow"')
-        return streamdeck_imageFromText(output, {['fontSize'] = 40 })
+        local justTemperature = output:gsub('%W', '')
+        local value = tonumber(justTemperature) / 100.0
+        local color = colorBetween(hs.drawing.color.blue, hs.drawing.color.red, value)
+        local options = {
+            ['backgroundColor'] = color,
+            ['textColor'] = hs.drawing.color.black,
+            ['fontSize'] = 40
+        }
+        return streamdeck_imageFromText(output, options)
     end,
     ['updateInterval'] = 60 * 60 * 10,
 })
