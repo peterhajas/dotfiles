b03bda36bb1eb4485306cf71473a04c508d44c03
Peter Hajas <peterhajas@gmail.com>
2020-11-21 01:42:58 -0600

    Change out apps that I install

--- osx_cask.bash
+++ osx_cask.bash
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 brew install caskroom/cask/brew-cask
-brew cask install google-chrome
 brew cask install macvim
 brew cask install vlc
 brew cask install audacity
 brew cask install sketch
 brew cask install xquartz
 brew cask install hex-fiend
-brew cask install appcleaner
 brew cask install daisydisk
 brew cask install audioscrobbler
 brew cask install bitbar
-brew cask install bowtie
 brew cask install karabiner-elements
 brew cask install hammerspoon
-brew cask install bartender
+brew cask install blender
+brew cask install gnucash
