ffbac8bd32c1feb21938a8741f7648afa0c1fa2a
Peter Hajas <peterhajas@gmail.com>
2021-02-12 17:25:28 -0800

    Cache the canvas

--- hammerspoon/.hammerspoon/streamdeck_buttons/button_images.lua
+++ hammerspoon/.hammerspoon/streamdeck_buttons/button_images.lua
@@ -1,20 +1,22 @@
 buttonWidth = 96
 buttonHeight = 96
 
+-- Shared cached canvas
+local sharedCanvas = hs.canvas.new{ w = buttonWidth, h = buttonHeight }
+
 -- Returns an image with the specified text, color, and background color
 function streamdeck_imageFromText(text, options)
-    local imageCanvas = hs.canvas.new{ w = buttonWidth, h = buttonHeight }
     local options = options or { }
     textColor = options['textColor'] or hs.drawing.color.white
     backgroundColor = options["backgroundColor"] or hs.drawing.color.black
     fontSize = options['fontSize'] or 70
-    imageCanvas[1] = {
+    sharedCanvas[1] = {
         action = "fill",
         frame = { x = 0, y = 0, w = buttonWidth, h = buttonHeight },
         fillColor = backgroundColor,
         type = "rectangle",
     }
-    imageCanvas[2] = {
+    sharedCanvas[2] = {
         frame = { x = 0, y = 0, w = buttonWidth, h = buttonHeight },
         text = hs.styledtext.new(text, {
             font = { name = ".AppleSystemUIFont", size = fontSize },
@@ -23,5 +25,5 @@ function streamdeck_imageFromText(text, options)
         }),
         type = "text",
     }
-    return imageCanvas:imageFromCanvas()
+    return sharedCanvas:imageFromCanvas()
 end
