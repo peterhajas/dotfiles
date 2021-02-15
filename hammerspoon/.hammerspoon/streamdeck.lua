db86bb8a3a4cd489f024a7fdf9118e451de0826b
Peter Hajas <peterhajas@gmail.com>
2021-02-14 20:43:27 -0800

    Remove button documentation from streamdeck.lua

--- hammerspoon/.hammerspoon/streamdeck.lua
+++ hammerspoon/.hammerspoon/streamdeck.lua
@@ -64,17 +64,6 @@ local function updateButton(i, pressed)
     profileStop('streamdeckButtonUpdate_' .. i)
 end
 
--- Buttons are defined as tables, with some values:
--- 'image': the image
--- 'imageProvider': the function returning the image, taking some context
--- 'pressDown': the function to perform on press down
--- 'pressUp': the function to perform on press up
--- 'updateInterval': the desired update interval (if any) in seconds
--- 'name': the name of the button
--- 'children': function returning child buttons, which will be pushed
-
--- Internal values:
--- '_timer': the timer that is updating this button
 
 -- Disables all timers for all buttons
 local function disableTimers()
