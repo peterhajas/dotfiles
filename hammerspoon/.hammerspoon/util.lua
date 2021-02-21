82135415bec361ee4ea0644e0555ba6614c88220
Peter Hajas <peterhajas@gmail.com>
2021-02-20 20:03:49 -0800

    Table merging function

--- hammerspoon/.hammerspoon/util.lua
+++ hammerspoon/.hammerspoon/util.lua
@@ -31,6 +31,22 @@ function cloneTable(oldTable)
     return newTable
 end
 
+-- https://stackoverflow.com/questions/1283388/lua-merge-tables
+function mergeTables(t1, t2)
+    for k,v in pairs(t2) do
+        if type(v) == "table" then
+            if type(t1[k] or false) == "table" then
+                mergeTables(t1[k] or {}, t2[k] or {})
+            else
+                t1[k] = v
+            end
+        else
+            t1[k] = v
+        end
+    end
+    return t1
+end
+
 -- Debug print
 function dbg(string)
     local debugLogger = hs.logger.new('debug', 'debug')
