d50fad9da000be7cb33a8ebd70c78068ba89cdb1
Peter Hajas <peterhajas@gmail.com>
2021-03-07 23:26:38 -0800

    Improve the HA icons more

--- hammerspoon/.hammerspoon/streamdeck_util/ha_mdi_icon.lua
+++ hammerspoon/.hammerspoon/streamdeck_util/ha_mdi_icon.lua
@@ -30,7 +30,7 @@ local function typeFor(entityDictionary)
     return entityType
 end
 
-local function colorFor(entityDictionary)
+local function fallbackColorFor(entityDictionary)
     -- If we have a color set, let's use that
     local entityColor = entityDictionary['attributes']['rgb_color']
     if entityColor ~= nil then
@@ -58,11 +58,44 @@ local function colorFor(entityDictionary)
     return hs.drawing.color.white
 end
 
-local function nameFor(entityDictionary)
-    local entityID = entityIDFor(entityDictionary)
+local function colorSetFor(entityDictionary, key, fallback)
+    local color = entityDictionary['attributes'][key]
+    if color ~= nil then
+        if string.find(color, '#') then
+            return { ['hex'] = color }
+        else
+            local x11Color = hs.drawing.color.lists()['x11'][color]
+            if x11Color ~= nil then
+                return x11Color
+            end
+        end
+    end
+    return fallback
+end
+
+local function textColorFor(entityDictionary)
+    return colorSetFor(entityDictionary, 'text_color', fallbackColorFor(entityDictionary))
+end
+
+local function iconColorFor(entityDictionary)
+    return colorSetFor(entityDictionary, 'icon_color', fallbackColorFor(entityDictionary))
+end
+
+local function backgroundColorFor(entityDictionary)
+    return colorSetFor(entityDictionary, 'background_color', hs.drawing.color.black)
+end
+
+local function titleFor(entityDictionary)
     local name = entityDictionary['attributes']['friendly_name']
     if name == nil then
+        local entityID = entityIDFor(entityDictionary)
         name = entityID
+    end
+    local entityType = typeFor(entityDictionary)
+    if entityType == 'person' then
+        local location = entityDictionary['state']
+        location = location:gsub('_', ' ')
+        name = location
     end
     return name
 end
@@ -96,14 +129,16 @@ function homeAssistantEntityIcon(entityDictionary)
     end
 
     options = { }
-    textColor = colorFor(entityDictionary)
-    backgroundColor = hs.drawing.color.black
+    local textColor = textColorFor(entityDictionary)
+    local iconColor = iconColorFor(entityDictionary)
+    local backgroundColor = backgroundColorFor(entityDictionary)
 
     -- Flip if we're on
     if entityDictionary['state'] == 'on' then
         local newTextColor = backgroundColor
         backgroundColor = textColor
         textColor = newTextColor
+        iconColor = newTextColor
     end
 
     local elements = { }
@@ -121,8 +156,9 @@ function homeAssistantEntityIcon(entityDictionary)
     if entityPicture ~= nil then
         table.insert(elements, {
             type = "image",
-            frame = imageFrame,
+            frame = { x = 0, y = 0, w = buttonWidth, h = buttonHeight },
             image = entityPicture,
+            imageScaling = 'scaleToFit',
         })
     else
         -- Find the MDI glyph
@@ -139,7 +175,7 @@ function homeAssistantEntityIcon(entityDictionary)
                 text = hs.styledtext.new(mdi, {
                     font = { name = 'MaterialDesignIcons', size = 50 },
                     paragraphStyle = { alignment = "center" },
-                    color = textColor,
+                    color = iconColor,
                 }),
             })
         else
@@ -149,7 +185,7 @@ function homeAssistantEntityIcon(entityDictionary)
                 text = hs.styledtext.new('•', {
                     font = { name = '.AppleSystemUIFont', size = 50 },
                     paragraphStyle = { alignment = "center" },
-                    color = textColor,
+                    color = iconColor,
                 }),
             })
         end
@@ -158,7 +194,7 @@ function homeAssistantEntityIcon(entityDictionary)
     -- Name
     table.insert(elements, {
         frame = { x = 0, y = buttonHeight - textHeight, w = buttonWidth, h = textHeight },
-        text = hs.styledtext.new(nameFor(entityDictionary), {
+        text = hs.styledtext.new(titleFor(entityDictionary), {
             font = { name = '.AppleSystemUIFont', size = 15 },
             paragraphStyle = { alignment = "center" },
             color = textColor,
