a0c20aec77f87a32788938c54b3829586315dea4
Peter Hajas <peterhajas@gmail.com>
2021-03-09 21:29:27 -0800

    Improve youtubedl when using Reeder

--- hammerspoon/.hammerspoon/youtubedl.lua
+++ hammerspoon/.hammerspoon/youtubedl.lua
@@ -3,11 +3,20 @@ require "terminal"
 
 -- requires youtube-dl and my ytd script
 
+local function youtubedl(url)
+    local command = "ytd \""..url.."\""
+    runInNewTerminal(command, true)
+end
+
 hs.hotkey.bind(hyper, "w", function()
+    -- If Reeder is running, strip the URL
+    local reeder = hs.application('com.reederapp.5.macOS')
+    if reeder ~= nil then
+        reeder:selectMenuItem("Copy Link")
+    end
     -- Grab pasteboard
     local pasteboard = hs.pasteboard.readString()
     if string.find(pasteboard, 'http') then
-        local command = "ytd \""..pasteboard.."\""
-        runInNewTerminal(command, true)
+        youtubedl(pasteboard)
     end
 end)
