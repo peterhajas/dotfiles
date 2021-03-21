2e219aa82fa01b69f214efd5f25cc4d53acc3557
Peter Hajas <peterhajas@gmail.com>
2021-03-21 16:02:24 -0700

    Make shell a bit terser, drop leading space

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,7 +1,4 @@
 function fish_prompt
-    set_color cyan; echo -n (prompt_current_user)
-    set_color normal
-    echo -n ' '
     set_color green; echo -n (prompt_pwd)
     echo -n ' '
     prompt_char
