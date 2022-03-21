c85e00c7f6b5e093de46dcc02cd501908e0d7e1f
Peter Hajas <peter@haj.as>
2022-03-20 21:29:22 -0600

    Read ha_url and ha_token out of secrets

--- home_assistant/bin/home_assistant_run.bash
+++ home_assistant/bin/home_assistant_run.bash
@@ -3,6 +3,6 @@
 METHOD=$1
 ENDPOINT=$2
 PARAMETERS=$3
-BEARER="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI3MGRiZTVhMGZmZGU0MTFhOTY5MzI0NjM0YTYzNmY5YyIsImlhdCI6MTYxMDYwODIxNiwiZXhwIjoxOTI1OTY4MjE2fQ.cLQX5-u71GgxlGkXXVXSpt0ZQ4IS9Kz9coqAVXbefHw"
+BEARER=$ha_token
 
-curl -X $METHOD --data "$PARAMETERS" -H "Authorization: Bearer $BEARER" -H "Content-Type: application/json" --silent http://beacon:8123/api/$ENDPOINT
+curl -X $METHOD --data "$PARAMETERS" -H "Authorization: Bearer $BEARER" -H "Content-Type: application/json" --silent $ha_url/api/$ENDPOINT
