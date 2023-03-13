e9fdcf36293b301ef7b025d65da303bac67bf49c
Peter Hajas <peter@haj.as>
2023-03-12 21:08:49 -0600

    Change app shortcuts - no more option ones

--- hammerspoon/.hammerspoon/app_shortcuts.lua
+++ hammerspoon/.hammerspoon/app_shortcuts.lua
@@ -3,33 +3,33 @@ require('util')
 -- App Shortcuts {{{
 -- Option {{{
 
--- Option-M for Mail
+-- -- Option-M for Mail
 
-hs.hotkey.bind({"alt"}, "m", function()
-    hs.application.launchOrFocus("Mail")
-end)
+-- hs.hotkey.bind({"alt"}, "m", function()
+--     hs.application.launchOrFocus("Mail")
+-- end)
 
--- Option-A for Messages
+-- -- Option-A for Messages
 
-hs.hotkey.bind({"alt"}, "a", function()
-    hs.application.launchOrFocus("Messages")
-end)
+-- hs.hotkey.bind({"alt"}, "a", function()
+--     hs.application.launchOrFocus("Messages")
+-- end)
 
--- Option-Tab for Terminal
+-- -- Option-Tab for Terminal
 
-hs.hotkey.bind({"alt"}, "tab", function()
-    hs.application.launchOrFocus("Terminal")
-end)
+-- hs.hotkey.bind({"alt"}, "tab", function()
+--     hs.application.launchOrFocus("Terminal")
+-- end)
 
--- Option-R for Reeder
-hs.hotkey.bind({"alt"}, "r", function()
-    hs.application.launchOrFocus("Reeder")
-end)
+-- -- Option-R for Reeder
+-- hs.hotkey.bind({"alt"}, "r", function()
+--     hs.application.launchOrFocus("Reeder")
+-- end)
 
--- Option-H for HomeAssistant
-hs.hotkey.bind({"alt"}, "h", function()
-    hs.application.launchOrFocus("Home Assistant")
-end)
+-- -- Option-H for HomeAssistant
+-- hs.hotkey.bind({"alt"}, "h", function()
+--     hs.application.launchOrFocus("Home Assistant")
+-- end)
 
 -- }}}
 -- Hyper {{{
@@ -40,6 +40,8 @@ local hyperAppShortcuts = {
     ['f'] = 'Terminal',
     ['g'] = 'Mail',
     ['c'] = 'Calendar',
+    ['b'] = 'Home Assistant',
+    ['r'] = 'Reeder',
 }
 
 for shortcut,app in pairs(hyperAppShortcuts) do
