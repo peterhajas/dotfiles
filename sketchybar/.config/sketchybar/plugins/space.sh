dfeba0e6fe837ae1d179e5a84c4e92bf704e76e6
Peter Hajas <peter@haj.as>
2023-04-01 21:30:33 -0400

    updated space names

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -1,7 +1,7 @@
 # first is empty
-SPACENAMES=('' 'main' 'sub' 'comms' 'rss' 'ha')
+SPACENAMES=('' 'main' 'sub' 'comm' 'rss' 'ha')
 SPACENAME="${SPACENAMES[$SID]}"
-UNREADCOUNT=$(yabai -m query --windows --space $SID | jq -r ".[] | .app" | sort | uniq | xargs -L1 lsappinfo info -only StatusLabel -app |grep label | awk '{split($0,a,"StatusLabel"); label=a[2]; print label }' | cut -c 3- | sed 's/=/:/' | jq -r ".label")
+UNREADCOUNT=$(yabai -m query --windows --space $SID | jq -r ".[] | .app" | sort | uniq | xargs -L1 lsappinfo info -only StatusLabel -app |grep label | awk '{split($0,a,"StatusLabel"); label=a[2]; print label }' | cut -c 3- | sed 's/=/:/' | jq -r ".label" | awk ' $0 > 0 { print "*"; }')
 sketchybar --set $NAME label="$SPACENAME $UNREADCOUNT"
 sketchybar --set $NAME background.drawing=$SELECTED
 sketchybar --set $NAME label.highlight=$SELECTED
