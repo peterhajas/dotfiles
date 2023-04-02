e6883be970a2c11a2545bcbeefb4c4528507215f
Peter Hajas <peter@haj.as>
2023-04-02 09:36:31 -0400

    space and window color updates

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -1,5 +1,5 @@
 # first is empty
-SPACENAMES=('' 'main' 'sub' 'comm' 'rss' 'ha')
+SPACENAMES=('' 'main' 'term' 'comm' 'rss' 'ha' 'fmf')
 SPACENAME="${SPACENAMES[$SID]}"
 UNREADCOUNT=$(yabai -m query --windows --space $SID | jq -r ".[] | .app" | sort | uniq | xargs -L1 lsappinfo info -only StatusLabel -app |grep label | awk '{split($0,a,"StatusLabel"); label=a[2]; print label }' | cut -c 3- | sed 's/=/:/' | jq -r ".label" | awk ' $0 > 0 { print "*"; }')
 sketchybar --set $NAME label="$SPACENAME$UNREADCOUNT"
--- sketchybar/.config/sketchybar/plugins/window.sh
+++ sketchybar/.config/sketchybar/plugins/window.sh
@@ -1,1 +1,1 @@
-sketchybar --set $NAME label="$(yabai -m query --windows --window | jq --raw-output '.title')"
+sketchybar --set $NAME label="$(yabai -m query --windows --window | jq --raw-output '.title' | awk '{ if (length($0) > 64) { print substr($0, 1, 64) "..." } else { print $0 } }')"
