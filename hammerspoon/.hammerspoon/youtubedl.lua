82329fd3f2e0cf1fc97e30eee32d54d919a513de
Peter Hajas <peter@haj.as>
2023-04-04 09:18:22 -0400

    remove reeder integration from youtube dl hotkey

--- hammerspoon/.hammerspoon/youtubedl.lua
+++ hammerspoon/.hammerspoon/youtubedl.lua
@@ -9,11 +9,6 @@ local function youtubedl(url)
 end
 
 hs.hotkey.bind(hyper, "w", function()
-    -- If Reeder is running, strip the URL
-    local reeder = hs.application('com.reederapp.5.macOS')
-    if reeder ~= nil then
-        reeder:selectMenuItem("Copy Link")
-    end
     -- Grab pasteboard
     local pasteboard = hs.pasteboard.readString()
     if string.find(pasteboard, 'http') then
