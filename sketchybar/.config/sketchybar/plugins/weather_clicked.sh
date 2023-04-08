f84d4755e20cce4b06972432f3af5447f643b5f0
Peter Hajas <peter@haj.as>
2023-04-08 09:46:08 -0400

    add city customization (put them in ~/.phajas/weather)

---
+++ sketchybar/.config/sketchybar/plugins/weather_clicked.sh
@@ -0,0 +1,18 @@
+PLUGIN_DIR="$HOME/.config/sketchybar/plugins"
+
+sketchybar -m --set weather popup.drawing=toggle
+
+sketchybar --remove "/weather.details.*/"
+
+CITIES=$(cat ~/.phajas/weather)
+
+for CITY in $CITIES
+do
+    NAME="weather.details.$CITY"
+    sketchybar --add item $NAME popup.weather
+    sketchybar --set $NAME label="$CITY: (loading)"
+    sketchybar --set $NAME script="$PLUGIN_DIR/weather_city.sh"
+    sketchybar --subscribe $NAME weather_update
+done
+
+sketchybar --trigger weather_update
