6bbc643512ab53ab14fd53d08210d12b996a33e2
Peter Hajas <peter@haj.as>
2023-09-24 11:58:55 -0600

    search for hidden files with telescope, too

--- nvim/.config/nvim/lua/phajas/plugins/telescope.lua
+++ nvim/.config/nvim/lua/phajas/plugins/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ hidden = true }) end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
