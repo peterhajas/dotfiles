b1d974bb9284f8d290bc43d68138ccfe5722d17e
Peter Hajas <peter@haj.as>
2023-09-24 11:58:39 -0600

    also allow ctrl-arrow keys to move buffer focus

--- nvim/.config/nvim/lua/phajas/remap.lua
+++ nvim/.config/nvim/lua/phajas/remap.lua
@@ -10,6 +10,10 @@ vim.keymap.set("n", "<left>", "<C-w>h")
 vim.keymap.set("n", "<right>", "<C-w>l")
 vim.keymap.set("n", "<up>", "<C-w>k")
 vim.keymap.set("n", "<down>", "<C-w>j")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
 
 -- Tabs
 vim.keymap.set("n", "<leader>t", vim.cmd.tabnew)
