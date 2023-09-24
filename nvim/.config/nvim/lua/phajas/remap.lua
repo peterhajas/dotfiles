1696b757a91a779920c1e50a4aa58bf07bea2a3f
Peter Hajas <peter@haj.as>
2023-09-24 14:00:41 -0600

    some terminal key commands and bindings

--- nvim/.config/nvim/lua/phajas/remap.lua
+++ nvim/.config/nvim/lua/phajas/remap.lua
@@ -20,6 +20,8 @@ vim.keymap.set("n", "<leader>t", vim.cmd.tabnew)
 -- plh-evil: why aren't these ]t and [t? some unimpaired.vim collision?
 vim.keymap.set("n", "]w", vim.cmd.tabnext)
 vim.keymap.set("n", "[w", vim.cmd.tabprevious)
+vim.keymap.set("t", "]w", vim.cmd.tabnext)
+vim.keymap.set("t", "[w", vim.cmd.tabprevious)
 
 -- Folds
 vim.keymap.set("n", "<leader>f", "za")
@@ -38,3 +40,18 @@ vim.keymap.set("n", "<BS>", "%")
 -- Move selected text around (thanks theprimeagen)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Terminal
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- Opens a new terminal in a split
+-- plh-evil: it'd be cool if each tab had one of these, and this function was
+-- smart enough to "jump" there if we hit it again...
+local function terminal()
+    vim.cmd(vim.api.nvim_replace_termcodes("normal <C-w>s<C-w>j", true, true, true))
+    vim.cmd(":terminal")
+    vim.cmd(vim.api.nvim_replace_termcodes("normal A", true, true, true))
+end
+
+vim.keymap.set("n", "<leader><CR>", function() terminal() end)
+vim.keymap.set("t", "<leader><CR>", function() terminal() end)
