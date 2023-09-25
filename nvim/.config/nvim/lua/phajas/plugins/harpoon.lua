b0915700cd69eb7f6a25814d61a0fed6c09a9d00
Peter Hajas <peter@haj.as>
2023-09-25 07:00:50 -0600

    better harpoon keycommands

--- nvim/.config/nvim/lua/phajas/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/phajas/plugins/harpoon.lua
@@ -2,6 +2,10 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>A", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>h", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>j", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>k", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>l", function() ui.nav_file(4) end)
 
 -- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
