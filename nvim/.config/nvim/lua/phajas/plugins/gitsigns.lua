e0b6e0978513fadbd6a5d79d07208279440778fc
Peter Hajas <peter@haj.as>
2023-10-01 15:35:19 -0600

    no need for custom characters in gitsigns - easy to do with catppuccin

--- nvim/.config/nvim/lua/phajas/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/phajas/plugins/gitsigns.lua
@@ -1,10 +1,10 @@
 require("gitsigns").setup {
   signs = {
-    add          = { text = '+' },
-    change       = { text = '•' },
-    delete       = { text = '-' },
-    topdelete    = { text = '‾' },
-    changedelete = { text = '~' },
-    untracked    = { text = '?' },
+    add          = { text = '|' },
+    change       = { text = '|' },
+    delete       = { text = '|' },
+    topdelete    = { text = '|' },
+    changedelete = { text = '|' },
+    untracked    = { text = '|' },
   },
 }
