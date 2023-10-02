a924f350b95f33c6151f2d3ae2d0a3f9d57e44df
Peter Hajas <peter@haj.as>
2023-10-01 20:18:52 -0600

    only apply catppuccin if we're not running in Terminal.app

--- nvim/.config/nvim/lua/phajas/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/phajas/plugins/catppuccin.lua
@@ -1,9 +1,18 @@
-require("catppuccin").setup({
-    flavour = "mocha",
-    integrations = {
-        harpoon = true,
-        treesitter_context = false,
-    }
-})
+-- catppuccin does not work well for Terminal.app, so only enable it if we're
+-- not running in Terminal
+--
+-- macOS sets this environment variable for apps, so check the bundle ID or
+-- enable if it is `nil`
+local bundleID = os.getenv("__CFBundleIdentifier")
+if bundleID == nil or
+   bundleID ~= "com.apple.Terminal" then
+    require("catppuccin").setup({
+        flavour = "mocha",
+        integrations = {
+            harpoon = true,
+            treesitter_context = false,
+        }
+    })
 
-vim.cmd.colorscheme "catppuccin"
+    vim.cmd.colorscheme "catppuccin"
+end
