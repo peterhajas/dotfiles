3c37bd663635dbe05ae065275538d19ebc29fab3
Peter Hajas <peter@haj.as>
2023-10-06 21:33:10 -0600

    better telescope bindings, add one for hidden files

--- nvim/.config/nvim/lua/phajas/plugins/telescope.lua
+++ nvim/.config/nvim/lua/phajas/plugins/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pF', function() builtin.find_files({ hidden = true }) end)
+vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 
