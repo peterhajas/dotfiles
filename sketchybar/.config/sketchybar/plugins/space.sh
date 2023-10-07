d06920a45424b483b02bb563babb7e30edbb456c
Peter Hajas <peter@haj.as>
2023-10-07 14:06:25 -0600

    more spaces in sketchybar

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -1,5 +1,6 @@
 # first is empty
-SPACENAMES=('' 'main' 'term' 'comm' 'rss' 'ha' 'fmf' 'watch')
+# last is 0 to give keyboard similarity
+SPACENAMES=('' 'main' 'term' 'comm' 'rss' 'ha' 'fmf' 'watch' '8' '9' '0')
 SPACENAME="${SPACENAMES[$SID]}"
 UNREADCOUNT=$(yabai -m query --windows --space $SID | jq -r ".[] | .app" | sort | uniq | xargs -L1 lsappinfo info -only StatusLabel -app |grep label | awk '{split($0,a,"StatusLabel"); label=a[2]; print label }' | cut -c 3- | sed 's/=/:/' | jq -r ".label" | awk ' $0 > 0 { print "*"; }')
 sketchybar --set $NAME label="$SPACENAME$UNREADCOUNT"
