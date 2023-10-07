e8cde814fb270596eef3bdd77dd14ae29dfbd407
Peter Hajas <peter@haj.as>
2023-10-06 23:38:49 -0600

    better cmp keybindings that don't fight me

--- nvim/.config/nvim/lua/phajas/plugins/cmp.lua
+++ nvim/.config/nvim/lua/phajas/plugins/cmp.lua
@@ -9,11 +9,11 @@ cmp.setup({
         documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ['j'] = cmp.mapping.select_next_item(),
-        ['k'] = cmp.mapping.select_prev_item(),
-        ['J'] = cmp.mapping.scroll_docs(4),
-        ['K'] = cmp.mapping.scroll_docs(-4),
-        ['<left>'] = cmp.mapping.close(),
+        ['<down>'] = cmp.mapping.select_next_item(),
+        ['<up>'] = cmp.mapping.select_prev_item(),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<esc>'] = cmp.mapping.close(),
         ['<right>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
