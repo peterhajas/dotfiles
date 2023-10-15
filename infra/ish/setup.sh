5ddc6f68eac0c82e9522348156214dee2081a401
Peter Hajas <peter@haj.as>
2023-10-15 10:23:58 -0600

    add more to infra script

--- infra/ish/setup.sh
+++ infra/ish/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-echo "doing apk update..."
+echo "=== doing apk update..."
 apk update
-echo "doing apk upgrade..."
+echo "=== doing apk upgrade..."
 apk upgrade
-echo "installing neovim..."
+echo "=== installing neovim..."
 apk add neovim
 apk add neovim-doc
+echo "=== installing git..."
+apk add git
+echo "=== cloning repo..."
+git clone peterhajas.com:phajas-wiki ~/phajas-wiki
