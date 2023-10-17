ebc25f524c87bacafaff83d7630a8d0daebd271c
Peter Hajas <peter@haj.as>
2023-10-17 06:55:13 -0600

    add navigation entry for opening a file at the cursor

--- nvim/.config/nvim/lua/phajas/wiki.lua
+++ nvim/.config/nvim/lua/phajas/wiki.lua
@@ -3,55 +3,19 @@ History = require("phajas.wiki_history")
 Info = require("phajas.wiki_info")
 Paths = require("phajas.wiki_paths")
 
-local function DEBUG(thing)
-    vim.api.nvim_echo({ { vim.inspect( thing) } }, true, {})
-end
-
-local function WikiBufferOpenFileAtCursor(winno, bufno)
-    local row, column = unpack(vim.api.nvim_win_get_cursor(winno))
-    local line = vim.api.nvim_buf_get_lines(bufno, row-1, row, false)[1]
-    local startIndex = nil
-    local endIndex = nil
-    local title = nil
-    startIndex, endIndex = 1, 1
-
-    -- Find where square brackets are, if anywhere
-    while startIndex and endIndex do
-        startIndex, endIndex = string.find(line, "%[%[", endIndex)
-        if startIndex and endIndex then
-            local startBrackets, endBrackets = string.find(line, "%]%]", endIndex)
-            if startBrackets and endBrackets then
-                title = string.sub(line, startIndex, endBrackets)
-                endIndex = endBrackets + 1
-                break
-            end
-        end
-    end
-
-    if startIndex ~= nil and endIndex ~= nil and
-       startIndex < column+2 and endIndex > column-2 and
-       title ~= nil then
-        title = string.gsub(title, "%[", "")
-        title = string.gsub(title, "%]", "")
-        Navigation.NavigateToFile(winno, bufno, title)
-    else
-        print("Not yet implemented") -- plh-evil: fix me, find selected text
-    end
-end
-
 local function WikiBufferEnter(info)
     local bufno = info.buf
 
     vim.api.nvim_buf_set_keymap(bufno, 'n', '<CR>', '', {
         callback = function()
             local winno = vim.api.nvim_get_current_win()
-            WikiBufferOpenFileAtCursor(winno, bufno)
+            Navigation.OpenFileAtCursor(winno, winno, bufno)
         end
     })
     vim.api.nvim_buf_set_keymap(bufno, 'v', '<CR>', '', {
         callback = function()
             local winno = vim.api.nvim_get_current_win()
-            WikiBufferOpenFileAtCursor(winno, bufno)
+            Navigation.OpenFileAtCursor(winno, winno, bufno)
         end
     })
     vim.api.nvim_buf_set_keymap(bufno, 'n', '<BS>', '', {
--- nvim/.config/nvim/lua/phajas/wiki_navigation.lua
+++ nvim/.config/nvim/lua/phajas/wiki_navigation.lua
@@ -39,4 +39,42 @@ function N.GoBack(winno)
     end
 end
 
+function N.OpenFileAtCursor(winno, intoWinno, bufno)
+    local row, column = unpack(vim.api.nvim_win_get_cursor(winno))
+    local line = vim.api.nvim_buf_get_lines(bufno, row-1, row, false)[1]
+    local startIndex = nil
+    local endIndex = nil
+    local title = nil
+    startIndex, endIndex = 1, 1
+
+    -- Find where square brackets are, if anywhere
+    while startIndex and endIndex do
+        startIndex, endIndex = string.find(line, "%[%[", endIndex)
+        if startIndex and endIndex then
+            local startBrackets, endBrackets = string.find(line, "%]%]", endIndex)
+            if startBrackets and endBrackets then
+                title = string.sub(line, startIndex, endBrackets)
+                endIndex = endBrackets + 1
+                break
+            end
+        end
+    end
+
+    if startIndex ~= nil and endIndex ~= nil and
+        startIndex < column+2 and endIndex > column-2 and
+        title ~= nil then
+        title = string.gsub(title, "%[", "")
+        title = string.gsub(title, "%]", "")
+        Navigation.NavigateToFile(intoWinno, bufno, title)
+    else
+        -- If there's just a filename on this line, then go with that
+        local lineFilePath = Paths.FilePath(line)
+        if lineFilePath ~= nil then
+            Navigation.NavigateToFile(intoWinno, bufno, line)
+        else
+            print("Not yet implemented") -- plh-evil: fix me, find selected text
+        end
+    end
+end
+
 return N
