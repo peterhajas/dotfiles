164b8db34d5955362e9c15bf11dda7e6a63ec011
Peter Hajas <peter@haj.as>
2023-10-19 07:27:40 -0600

    stop with the backlinking every time

--- nvim/.config/nvim/lua/phajas/nb.lua
+++ nvim/.config/nvim/lua/phajas/nb.lua
@@ -27,13 +27,8 @@ vim.keymap.set('n', '<leader>j', function() NBJournal() end, {})
 vim.api.nvim_create_autocmd({"BufWritePost"}, {
     group = vim.api.nvim_create_augroup("phajas-nb", { clear = true }),
     pattern = NBNotebookPath() .. "/*",
-    callback = function(opts)
-        -- We checkpoint here in case the backlinks operation borks something
+    callback = function()
         vim.fn.system("nb git checkpoint")
-        vim.fn.system("nb_backlinks_prune.py")
-        vim.fn.system("nb backlink --force")
-        vim.fn.system("nb git checkpoint")
-        vim.fn.system("nb sync 2>>/dev/null")
     end,
 })
 
