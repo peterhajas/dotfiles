030171600bd484ef18b8b70dd71024d135f3df01
Peter Hajas <peter@haj.as>
2023-10-19 05:32:10 -0600

    new link replace which uses farside

--- hammerspoon/.hammerspoon/link_replace.lua
+++ hammerspoon/.hammerspoon/link_replace.lua
@@ -1,23 +1,32 @@
 -- Replaces links that are found on the clipboard, if applicable
 
-local replacements = {
-    ['twitter.com'] = 'nitter.net',
-    ['/reddit.com'] = '/teddit.net',
-    ['old.reddit.com'] = 'teddit.net',
-    ['reddit.com'] = 'teddit.net',
-    ['instagram.com'] = 'bibliogram.art'
+local urlsToReplace = {
+    'imgur.com',
+    'instagram.com',
+    'medium.com',
+    'reddit.com',
+    'old.reddit.com',
+    'twitter.com',
+    'x.com',
 }
+
+local function startsWith(str, start)
+    return string.sub(str, 1, string.len(start)) == start
+end
 
 function replacePasteboardLinkIfNecessary(contents)
     if contents == nil then
         return
     end
 
-    for key, replacement in pairs(replacements) do
-        if string.find(contents, key) then
-            newContents = contents:gsub(key, replacement)
+    for _, rep in ipairs(urlsToReplace) do
+        local https = "https://" .. rep
+        local www = "https://www." .. rep
+        if startsWith(contents, https) or startsWith(contents, www) then
+            local newContents = "https://farside.link/" .. contents
             hs.pasteboard.setContents(newContents)
             hs.alert("Replaced")
+            return
         end
     end
 end
