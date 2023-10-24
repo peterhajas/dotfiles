b83c8015f4d369f393328eb53d20e6a1c28d9ad3
Peter Hajas <peter@haj.as>
2023-10-24 10:41:27 -0400

    whoops - forgot bufferline config

---
+++ nvim/.config/nvim/lua/phajas/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+require("bufferline").setup{
+    options = {
+        mode = "tabs",
+
+        show_buffer_close_icons = false,
+        show_buffer_icons = false,
+        show_close_icon = false,
+        show_tab_indicators = false,
+
+        separator_style = { '', '' },
+
+        always_show_bufferline = false,
+    }
+}
