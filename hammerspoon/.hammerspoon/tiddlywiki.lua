087587b228e3e4f338dd726fa42f9d3997aad062
Peter Hajas <peter@haj.as>
2023-11-04 15:39:11 -0700

    update tiddlywiki desktop preview and start using it

--- hammerspoon/.hammerspoon/tiddlywiki.lua
+++ hammerspoon/.hammerspoon/tiddlywiki.lua
@@ -6,30 +6,38 @@ end)
 local wikiDirectory = os.getenv("HOME") .. "/phajas-wiki/"
 local wikiPath = wikiDirectory .. "phajas-wiki.html"
 local webView = nil
+local tiddler = "Desktop%20Demo"
 
 local function update()
-    hs.alert("TICK")
-    webView:url("file://" .. wikiPath .. "#Heads%20Up%20Display")
-    webView:reload()
-
-    hs.timer.doAfter(5, update)
+    webView:url({
+        ["URL"] = "file://" .. wikiPath .. "#" .. tiddler,
+        ["cachePolicy"] = {
+            ["ignoreLocalCache"] = "true"
+        }
+    })
+    webView:evaluateJavaScript("document.body.style.zoom = 0.8")
+    hs.timer.doAfter(60, update)
 end
 
 local function layout()
-    local rect = hs.geometry.rect(0,1000,180,480)
+    local screen = hs.screen.primaryScreen()
+    local rect = hs.geometry.rect(0,20,180,screen:frame().h - 20)
     webView:frame(rect)
+end
+
+local wikiScreenWatcher = hs.screen.watcher.new(layout)
+
+local function setupWebView()
+    local rect = hs.geometry.rect(0,0,0,0)
+    webView = hs.webview.new(rect)
+    webView:behavior(hs.drawing.windowBehaviors.canJoinAllSpaces)
+    webView:transparent(true)
+    webView:sendToBack()
+    webView:show()
+    wikiScreenWatcher:start()
+
+    layout()
     update()
 end
 
-local function setupWebView()
-    local rect = hs.geometry.rect(0,1000,180,480)
-    webView = hs.webview.new(rect)
-    webView:url("file://" .. wikiPath .. "#Heads%20Up%20Display")
-    webView:behavior(hs.drawing.windowBehaviors.canJoinAllSpaces)
-    webView:show()
-
-    layout()
-    hs.timer.doAfter(1, update)
-end
-
--- setupWebView()
+setupWebView()
