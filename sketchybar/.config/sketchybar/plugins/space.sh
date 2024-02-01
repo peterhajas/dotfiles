5b2a11ca7e5b5079e1d65883f88aefbe7c0e5990
Peter Hajas <peter@haj.as>
2024-02-01 06:01:25 -0700

    more space names

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -1,6 +1,6 @@
 # first is empty
 # last is 0 to give keyboard similarity
-SPACENAMES=('' 'main' 'term' 'comm' 'rss' 'ha' 'fmf' 'watch' '8' '9' '0')
+SPACENAMES=('' 'main' 'term' 'comm' 'rss' 'ha' 'fmf' 'watch' 'bg' 'tmp' 'wiki')
 SPACENAME="${SPACENAMES[$SID]}"
 SPACEAPPS=$(yabai -m query --windows --space 1 | jq -r ".[] | .app" | uniq) #plh-evil: to be fixed up
 UNREADCOUNT=$(yabai -m query --windows --space $SID | jq -r ".[] | .app" | sort | uniq | xargs -L1 lsappinfo info -only StatusLabel -app |grep label | awk '{split($0,a,"StatusLabel"); label=a[2]; print label }' | cut -c 3- | sed 's/=/:/' | jq -r ".label" | awk ' $0 > 0 { print "*"; }')
