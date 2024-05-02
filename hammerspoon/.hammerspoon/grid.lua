483dc16a57fad79fd86935a651d9334a9b6e2c69
Peter Hajas <peter@haj.as>
2024-05-01 20:53:49 -0600

    update grid shortcut, make much smaller

--- hammerspoon/.hammerspoon/grid.lua
+++ hammerspoon/.hammerspoon/grid.lua
@@ -1,27 +1,8 @@
 require "hyper"
 
-function updateGridForScreen(screen)
-    -- The largest comfortable size to use for the keyboards I type on
-    size = hs.geometry.size(10, 4)
-
-    hs.grid.setGrid(size, screen)
-end
-
-function updateGridsForScreens()
-    screens = hs.screen.allScreens()
-    for i, screen in ipairs(screens) do
-        updateGridForScreen(screen)
-    end
-end
-
-updateGridsForScreens()
-
-local margins = hs.geometry.size(0, 0)
-
-hs.grid.setMargins(margins)
-hs.grid.ui.showExtraKeys = false
-
--- hs.hotkey.bind(hyper, ";", function()
---     hs.grid.toggleShow()
--- end)
+hs.hotkey.bind(hyper, ";", function()
+    hs.grid.ui.showExtraKeys = false
+    local margins = hs.geometry.point(0, 0)
+    hs.grid.setMargins(margins).setGrid(hs.geometry.size(10, 4)).toggleShow()
+end)
 
