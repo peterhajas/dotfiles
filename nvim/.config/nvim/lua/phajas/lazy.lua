dfd341f92c6aee5116d5de68ce631277e25d54c0
Peter Hajas <peter@haj.as>
2024-10-01 05:50:24 -0600

    add markdown preview

--- nvim/.config/nvim/lua/phajas/lazy.lua
+++ nvim/.config/nvim/lua/phajas/lazy.lua
@@ -54,6 +54,16 @@ local plugins = {
         'saadparwaiz1/cmp_luasnip',
     }},
 
+    -- Markdown
+    {"iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+
     -- AI
     {"olimorris/codecompanion.nvim", dependencies = {
         "nvim-lua/plenary.nvim",
