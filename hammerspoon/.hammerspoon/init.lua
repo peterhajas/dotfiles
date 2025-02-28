470dc3a9edd792205801a721cff611540be078bb
Peter Hajas <peter@haj.as>
2025-02-27 20:22:09 -0700

    tear down and restart DisplayLink manager on sleep

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -398,8 +398,11 @@ function caffeinateCallback(eventType)
         fluxSignificantTimeDidChange()
     elseif (eventType == hs.caffeinate.watcher.screensDidLock) then
         streamdeck_sleep()
+        hs.execute("osascript -e 'tell application \"DisplayLink Manager\" to quit'")
     elseif (eventType == hs.caffeinate.watcher.screensDidUnlock) then
         streamdeck_wake()
+        hs.application.open("DisplayLink Manager")
+
     end
 end
 
