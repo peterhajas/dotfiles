82014586c38365d91a2f3f112df33179fcfc0d12
Peter Hajas <peter@haj.as>
2025-08-01 20:39:41 -0600

    move from wezterm to ghostty

--- hammerspoon/.hammerspoon/terminal.lua
+++ hammerspoon/.hammerspoon/terminal.lua
@@ -4,7 +4,7 @@ function runInNewTerminal(command, exitAfterwards)
         effectiveCommand = command .. " && exit"
     end
 
-    hs.application.open("com.github.wez.wezterm")
+    hs.application.open("com.mitchellh.ghostty")
     hs.eventtap.keyStroke({"cmd"}, "n")
     hs.eventtap.keyStrokes(effectiveCommand)
     hs.eventtap.keyStroke({}, "return")
