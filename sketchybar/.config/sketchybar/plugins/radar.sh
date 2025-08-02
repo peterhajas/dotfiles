2513fe9e38ad6c7e55d46da56622720ceddae256
Peter Hajas <peter@haj.as>
2025-08-01 20:59:14 -0600

    also delete radar

--- sketchybar/.config/sketchybar/plugins/radar.sh
+++
@@ -1,6 +0,0 @@
-WIDTH=300
-wget --quiet --output-document "/tmp/radar.gif" "https://radar.weather.gov/ridge/standard/CONUS_9.gif"
-ffmpeg -i /tmp/radar.gif -vf scale=$WIDTH:-1 -y /tmp/radar.jpeg
-sketchybar --set $NAME width=$WIDTH
-sketchybar --set $NAME background.color=0x11000000
-sketchybar --set $NAME background.image="/tmp/radar.jpeg"
