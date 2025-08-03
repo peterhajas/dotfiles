d7500b1347cd0d35559eb408ba81eeb281c7122f
Peter Hajas <peter@haj.as>
2025-08-03 02:40:59 -0600

    remove some unused plugins

--- nvim/.config/nvim/lua/phajas/lazy.lua
+++ nvim/.config/nvim/lua/phajas/lazy.lua
@@ -65,17 +65,6 @@ local plugins = {
         ft = { "markdown" },
     },
 
-    -- AI
-    {"olimorris/codecompanion.nvim", dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
-        "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
-        { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves the default Neovim UI
-    },
-    config = true
-    },
-
     -- Harpoon
     {'theprimeagen/harpoon'},
 
