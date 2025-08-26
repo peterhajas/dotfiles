27c0724984305a9e6bc0252686c8d46a11c12d3f
Peter Hajas <peter@haj.as>
2025-08-25 21:45:04 -0600

    only one file search primitive, that works the same as the livegrep thingy

--- nvim/.config/nvim/lua/phajas/plugins/telescope.lua
+++ nvim/.config/nvim/lua/phajas/plugins/telescope.lua
@@ -2,8 +2,13 @@ local builtin = require('telescope.builtin')
 
 require('telescope').load_extension('fzf')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pF', function() builtin.find_files({ hidden = true }) end)
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({
+        hidden = true,
+        no_ignore = false,  -- This makes it respect .gitignore
+        find_command = { "fd", "--type", "f", "--hidden", "--exclude", ".git" }
+    })
+end)
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.live_grep({
