bad30a727c1986cb6f4deb3405d0d80908013131
Peter Hajas <peter@haj.as>
2025-08-25 21:20:21 -0600

    remove warnings from lsp.lua

--- nvim/.config/nvim/lua/phajas/plugins/lsp.lua
+++ nvim/.config/nvim/lua/phajas/plugins/lsp.lua
@@ -4,15 +4,15 @@ require('lspconfig').lua_ls.setup(lua_opts)
 
 lsp.preset("recommended")
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
     lsp.default_keymaps({buffer = bufnr})
 
     local opts = {buffer = bufnr, remap=false}
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1}) end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1}) end, opts)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
