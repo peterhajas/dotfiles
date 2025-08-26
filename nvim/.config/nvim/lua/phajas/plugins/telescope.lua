c19f90390c492a0fd84ca1a4b388ec839be9e902
Peter Hajas <peter@haj.as>
2025-08-25 21:42:08 -0600

    ps now searches files in hidden directories, but not in `.git`

--- nvim/.config/nvim/lua/phajas/plugins/telescope.lua
+++ nvim/.config/nvim/lua/phajas/plugins/telescope.lua
@@ -5,7 +5,13 @@ require('telescope').load_extension('fzf')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pF', function() builtin.find_files({ hidden = true }) end)
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.live_grep({
+        additional_args = function(args)
+            return vim.list_extend(args, {"--hidden", "--glob", "!.git"})
+        end
+    })
+end)
 
 vim.keymap.set('n', '<leader>gB', builtin.git_branches, {})
 
