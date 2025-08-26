406f2fdb0beab2036018582926c47abd22f5333b
Peter Hajas <peter@haj.as>
2025-08-25 22:11:06 -0600

    better inactive lualine

--- nvim/.config/nvim/lua/phajas/plugins/lualine.lua
+++ nvim/.config/nvim/lua/phajas/plugins/lualine.lua
@@ -10,7 +10,7 @@ require('lualine').setup {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
         lualine_c = {{'filename', path = 1}},
-        lualine_x = { '' },
+        lualine_x = {},
         lualine_y = {'progress'},
         lualine_z = {
             'location',
@@ -23,10 +23,18 @@ require('lualine').setup {
                 return ''
             end,
         }
-},
-extensions = {
-    'fugitive',
-    'fzf',
-    'oil',
-}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'location'}
+    },
+    extensions = {
+        'fugitive',
+        'fzf',
+        'oil',
+    }
 }
