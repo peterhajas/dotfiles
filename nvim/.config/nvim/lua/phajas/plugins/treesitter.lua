9a281056f4b9f50be4a2e57d2c715af484b34775
Peter Hajas <peter@haj.as>
2025-09-02 21:41:45 -0600

    treesitter format update

--- nvim/.config/nvim/lua/phajas/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/phajas/plugins/treesitter.lua
@@ -1,31 +1,31 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = {
-      "c",
-      "html",
-      "javascript",
-      "lua",
-      "luadoc",
-      "markdown",
-      "query",
-      "vim",
-      "vimdoc",
-  },
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = {
+        "c",
+        "html",
+        "javascript",
+        "lua",
+        "luadoc",
+        "markdown",
+        "query",
+        "vim",
+        "vimdoc",
+    },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  highlight = {
-    enable = true,
+    highlight = {
+        enable = true,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
 }
