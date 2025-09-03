35ab85dfbc003253bdb1c93a89878b6538165235
Peter Hajas <peter@haj.as>
2025-09-02 21:42:05 -0600

    add treesitter context

--- nvim/.config/nvim/lua/phajas/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/phajas/plugins/treesitter.lua
@@ -29,3 +29,7 @@ require'nvim-treesitter.configs'.setup {
         additional_vim_regex_highlighting = false,
     },
 }
+
+require('treesitter-context').setup({
+    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+})
