ae577c0dd9945b4ffb9a4c2273d940e73eb6b442
Peter Hajas <peter@haj.as>
2025-09-02 21:36:47 -0600

    add next / previous git hunk key command (unimpaired-style)

--- nvim/.config/nvim/lua/phajas/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/phajas/plugins/gitsigns.lua
@@ -1,10 +1,27 @@
-require("gitsigns").setup {
-  signs = {
-    add          = { text = '|' },
-    change       = { text = '|' },
-    delete       = { text = '|' },
-    topdelete    = { text = '|' },
-    changedelete = { text = '|' },
-    untracked    = { text = '|' },
-  },
+local gitsigns = require("gitsigns")
+gitsigns.setup {
+    signs = {
+        add          = { text = '|' },
+        change       = { text = '|' },
+        delete       = { text = '|' },
+        topdelete    = { text = '|' },
+        changedelete = { text = '|' },
+        untracked    = { text = '|' },
+    },
 }
+
+-- Next and previous git hunks, unimpaired-style
+-- This uses `vim.schedule` because I was noticing some refresh issues without
+-- that setting
+vim.keymap.set('n', ']g', function()
+    if vim.wo.diff then return ']c' end
+    vim.schedule(function() gitsigns.next_hunk() end)
+    return '<Ignore>'
+end, { expr = true, desc = "Next git hunk" })
+
+vim.keymap.set('n', '[g', function()
+    if vim.wo.diff then return '[c' end
+    vim.schedule(function() gitsigns.prev_hunk() end)
+    return '<Ignore>'
+end, { expr = true, desc = "Previous git hunk" })
+
