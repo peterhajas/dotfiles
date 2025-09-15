f65d139097c374b4a7789ae1b8b4ab90bc5afd0d
Peter Hajas <peter@haj.as>
2025-09-14 22:19:47 -0600

    enter to open / edit file in tree

--- nvim/.config/nvim/lua/phajas/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/phajas/plugins/nvim-tree.lua
@@ -4,6 +4,21 @@ require("nvim-tree").setup {
     update_focused_file = {
         enable = true
     },
+    on_attach = function(bufnr)
+        local function open_or_toggle()
+            local node = api.tree.get_node_under_cursor()
+            if node then
+                if node.type == 'file' then
+                    api.node.open.edit()
+                elseif node.type == 'directory' then
+                    api.node.open.edit()
+                end
+            end
+        end
+
+        -- Enter key to open files and toggle folders
+        vim.keymap.set('n', '<CR>', open_or_toggle, { buffer = bufnr, desc = "Open file or toggle folder" })
+    end,
 }
 
 vim.keymap.set('n', '<leader>s', function()
