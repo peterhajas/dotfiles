7c97060016ac0be73a70d28bcf8ce3fa6fae12c9
Peter Hajas <peter@haj.as>
2025-09-19 22:52:28 -0600

    remove dead nvim code

--- nvim/.config/nvim/lua/phajas/plugins/theme.lua
+++ nvim/.config/nvim/lua/phajas/plugins/theme.lua
@@ -1,23 +1,8 @@
--- catppuccin does not work well for Terminal.app, so only enable it if we're
+-- modus theme does not work well for Terminal.app, so only enable it if we're
 -- not running in Terminal
 --
 -- macOS sets this environment variable for apps, so check the bundle ID or
 -- enable if it is `nil`
-
-local function applyCatppuccin()
-    require("catppuccin").setup({
-        flavour = "auto",
-        integrations = {
-            cmp = true,
-            harpoon = true,
-            notify = true,
-            treesitter = false,
-            treesitter_context = false,
-        }
-    })
-
-    vim.cmd.colorscheme "catppuccin"
-end
 
 local function applyModus()
     vim.cmd([[colorscheme modus]])
