cd16fa5f2868402537fe96b84138eee6aab7832a
Peter Hajas <peter@haj.as>
2025-10-25 21:57:23 -0600

    default to default separators in lualine

--- nvim/.config/nvim/lua/phajas/plugins/lualine.lua
+++ nvim/.config/nvim/lua/phajas/plugins/lualine.lua
@@ -1,8 +1,6 @@
 require('lualine').setup {
     options = {
         icons_enabled = true,
-        component_separators = { left = ' ', right = ' '},
-        section_separators = { left = '', right = ''},
         theme = "auto",
     },
     sections = {
