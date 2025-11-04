5d1c8c606d6aa8fbd686ac04956106640aa952b1
Peter Hajas <peter@haj.as>
2025-11-04 06:10:23 -0700

    remove unused hammerspoon modules

--- hammerspoon/.hammerspoon/focus_follows_mouse.lua
+++
@@ -1,9 +0,0 @@
-hs.alert('hi')
-local mouseMovedEventTap = hs.eventtap.new({hs.eventtap.event.types.mouseMoved}, function(event)
-    hs.alert('event!')
-    local properties = event:properties()
-    local desc = hs.inspect.inspect(properties)
-    hs.alert(desc)
-end)
-
-mouseMovedEventTap:start()
