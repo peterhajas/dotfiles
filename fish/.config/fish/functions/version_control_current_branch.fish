0b6163571014acb9479ff697d9fbac4c0baf9062
Peter Hajas <peter@haj.as>
2025-11-07 23:31:56 -0700

    fix bugs in version_control_current_branch

--- fish/.config/fish/functions/version_control_current_branch.fish
+++ fish/.config/fish/functions/version_control_current_branch.fish
@@ -1,15 +1,17 @@
 function version_control_current_branch
     version_control_repo_type | read repo_type
-    
+
+    set branch ""
+
     switch $repo_type
         case git
-            git branch |grep "* " | awk '{print substr($0, 3)}'
+            git branch 2>/dev/null | grep "* " | awk '{print substr($0, 3)}' | read branch
         case svn
             # No idea if this works
-            svn info | sed -n "/URL:/s/.*\///p" | read branch
+            svn info 2>/dev/null | sed -n "/URL:/s/.*\///p" | read branch
         case hg
-            hg branch | read branch
-        end
+            hg branch 2>/dev/null | read branch
+    end
 
     echo -n $branch
 end
