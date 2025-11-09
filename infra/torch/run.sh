2ca57aa9b86b3013b6c5a43fd8c8a489e5a794f5
Peter Hajas <peter@haj.as>
2025-11-09 09:34:23 -0700

    jellyfin

--- infra/torch/run.sh
+++ infra/torch/run.sh
@@ -13,4 +13,12 @@ echo "Installing dependencies..."
 ansible-playbook dependencies.yml -i hosts -K
 
 echo ""
+echo "Configuring Podman storage..."
+ansible-playbook configure_podman_storage.yml -i hosts -K
+
+echo ""
+echo "Setting up Jellyfin..."
+ansible-playbook service_jellyfin.yml -i hosts
+
+echo ""
 echo "✓ All setup complete!"
