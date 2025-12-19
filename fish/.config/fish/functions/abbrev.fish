912560fdfeb186b293b2f3b89eb13dd6fd6865f5
Peter Hajas <peter@haj.as>
2025-12-18 23:23:44 -0700

    update `we` to use nvim plugin

--- fish/.config/fish/functions/abbrev.fish
+++ fish/.config/fish/functions/abbrev.fish
@@ -74,5 +74,5 @@ function abbrev -d "Install fish abbreviations"
     ## wiki
     abbr w "cd ~/phajas-wiki"
     abbr ww "tw ~/phajas-wiki/phajas-wiki.html "
-    abbr we "tw ~/phajas-wiki/phajas-wiki.html ls | fzf | xargs -I {} tw ~/phajas-wiki/phajas-wiki.html edit '{}'"
+    abbr we "nvim ~/phajas-wiki/phajas-wiki.html"
 end
