42b46b3b69a378100ab84e6c2396bb969f548f2d
Peter Hajas <peter@haj.as>
2025-12-18 23:21:52 -0700

    fix harpoon mappings

--- nvim/.config/nvim/lua/phajas/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/phajas/plugins/harpoon.lua
@@ -3,10 +3,9 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<leader>A", ui.toggle_quick_menu)
-vim.keymap.set("n", "<leader>j", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>j", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<leader>k", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<leader>l", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
 
 require("telescope").load_extension('harpoon')
-
