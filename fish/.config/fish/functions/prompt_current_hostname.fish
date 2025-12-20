5a5e2ccf98686fe864956be85f53f80dae7ef7d1
Peter Hajas <peter@haj.as>
2025-12-19 20:49:25 -0700

    improve SSH detection

--- fish/.config/fish/functions/prompt_current_hostname.fish
+++ fish/.config/fish/functions/prompt_current_hostname.fish
@@ -1,7 +1,6 @@
 function prompt_current_hostname
-    echo $SSH_TTY | wc -c | xargs | read SSH_TTY_BYTES
     # Only print the hostname if this is an SSH session
-    if test $SSH_TTY_BYTES -gt 1
+    if test -n "$SSH_CONNECTION"
         echo -n (hostname -s) | read HOSTNAME
         echo -n "$HOSTNAME "
     end
